8ac6acfda1154cdd63a2f6b9f56379450afe34d7
Juan Pablo Morales Ruiz <jmorales89@hotmail.cl>
2020-10-10 21:46:50 -0300

    first commit in rempo, django product table partial work

---
+++ backend/apps/main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Producto(models.Model):
+    id_producto = models.IntegerField(primary_key = True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300)
+
+
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'warehouse-jp-project'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('warehouse-jp-project');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('warehouse-jp-project app is running!');
+  });
+});
